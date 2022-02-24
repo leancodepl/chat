10cc2535d2e23b5771e1bcb070cf91a0c1ee9957
piotrdenert <piotr.denert@leancode.pl>
2022-02-24 16:38:49 +0100

    fix github actions

--- mobile/lib/src/presence/use_user_presence.dart
+++ mobile/lib/src/presence/use_user_presence.dart
@@ -7,7 +7,11 @@ import 'user_presence_synchronizer.dart';
 /// Tracks current user presence during the lifetime of the hook
 ///
 /// User is considered to be present when the app is open in the foreground.
-void useUserPresence(ChatClient client, WidgetsBinding widgetsBinding) {
+void useUserPresence(
+  ChatClient client,
+  WidgetsBinding widgetsBinding, {
+  List<Object?>? keys = const [],
+}) {
   useEffect(
     () {
       final synchronizer = UserPresenceSynchronizer(widgetsBinding, client)
@@ -15,6 +19,6 @@ void useUserPresence(ChatClient client, WidgetsBinding widgetsBinding) {
 
       return synchronizer.dispose;
     },
-    const [],
+    keys,
   );
 }
