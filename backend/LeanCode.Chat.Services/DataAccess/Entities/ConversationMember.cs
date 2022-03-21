8f016a5d4e9a1fc1d6f037fb54801042beb25222
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2022-03-21 18:37:16 +0100

    Fix race when "seeing" message that was sent right before my message

--- backend/LeanCode.Chat.Services/DataAccess/Entities/ConversationMember.cs
+++ backend/LeanCode.Chat.Services/DataAccess/Entities/ConversationMember.cs
@@ -5,51 +5,26 @@ namespace LeanCode.Chat.Services.DataAccess.Entities
 {
     public record ConversationMember
     {
-        public Guid LastDeliveredMessageId { get; private init; }
-        public DateTime LastDeliveredMessageDate { get; private init; }
         public Guid LastSeenMessageId { get; private init; }
         public DateTime LastSeenMessageDate { get; private init; }
 
         public ConversationMember(
-            Guid lastDeliveredMessageId,
-            DateTime lastDeliveredMessageDate,
             Guid lastSeenMessageId,
             DateTime lastSeenMessageDate)
         {
-            LastDeliveredMessageId = lastDeliveredMessageId;
-            LastDeliveredMessageDate = lastDeliveredMessageDate;
             LastSeenMessageId = lastSeenMessageId;
             LastSeenMessageDate = lastSeenMessageDate;
         }
 
-        public ConversationMember ForDeliveredMessage(Message m)
-        {
-            return this with
-            {
-                LastDeliveredMessageId = m.Id,
-                LastSeenMessageDate = m.DateSent,
-            };
-        }
-
         public ConversationMember ForSeenMessage(Message m)
         {
-            return this with
-            {
-                LastDeliveredMessageId = m.Id,
-                LastDeliveredMessageDate = m.DateSent,
-                LastSeenMessageId = m.Id,
-                LastSeenMessageDate = m.DateSent,
-            };
+            return new ConversationMember(m.Id, m.DateSent);
         }
 
         public static ConversationMember Empty()
         {
             var now = TimeProvider.Now;
-            return new ConversationMember(
-                Guid.Empty,
-                now,
-                Guid.Empty,
-                now);
+            return new ConversationMember(Guid.Empty, now);
         }
     }
 }
