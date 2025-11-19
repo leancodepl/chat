d48b0852b7d9b0836a7457b4e280f884952b7d1d
Michal Boguszewski <michal1842001@gmail.com>
2025-11-19 13:23:09 +0100

    Use NotificationDataConverter.ToNotificationData to map notification data

--- backend/LeanCode.Chat/Services/DataAccess/Services/NotificationsMapper.cs
+++ backend/LeanCode.Chat/Services/DataAccess/Services/NotificationsMapper.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using LeanCode.Chat.Contracts.Notifications;
+using LeanCode.Firebase.FCM;
 
 namespace LeanCode.Chat.Services.DataAccess.Services;
 
 public class NotificationsMapper
 {
+    private static readonly NotificationDataConverter notificationDataConverter = NotificationDataConverter.New().Build();
+
     public static Dictionary<string, string> ToNotificationData(NewMessageNotificationContentDTO content)
     {
-        var data = Firebase.FCM.Notifications.ToNotificationData(content);
+        var data = notificationDataConverter.ToNotificationData(content);
         data.Remove("Type");
 
         return data;
