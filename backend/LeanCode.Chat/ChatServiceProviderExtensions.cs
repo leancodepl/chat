da9a6b8ec0bc7cf1da6518cadfc5bfdebb79d909
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2026-01-12 11:06:05 +0100

    Add DI registration

--- backend/LeanCode.Chat/ChatServiceProviderExtensions.cs
+++ backend/LeanCode.Chat/ChatServiceProviderExtensions.cs
@@ -1,12 +1,17 @@
 using LeanCode.Chat.Services.DataAccess;
+using LeanCode.Chat.Services.DataAccess.Blobs;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LeanCode.Chat;
 
 public static class ChatServiceProviderExtensions
 {
-    public static void AddLeanChat(this IServiceCollection services)
+    public static void AddLeanChat(this IServiceCollection services, ChatAttachmentsConfiguration attachmentsConfig)
     {
         services.AddTransient<ChatService>();
+
+        services.AddSingleton(attachmentsConfig);
+        services.AddSingleton<BlobStorageDelegationKeyProvider>();
+        services.AddTransient<ChatAttachmentsBlobStorage>();
     }
 }
